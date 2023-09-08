00cc2ba1e748b21c43d352a96f7159109eb397ef
Migue <Migue@DESKTOP-GVDJK75>
2023-09-08 09:28:31 -0600

    primer cometario

---
+++ src/primerhola/Primerhola.java
@@ -0,0 +1,29 @@
+
+package primerhola;
+import java.util.Scanner;
+public class Primerhola {
+
+    public static void main(String[] args) {
+         Scanner teclado = new Scanner(System.in);
+        System.out.println("hola mundo");
+        System.out.println("como estamoooos");
+        int num, num2;
+
+        System.out.println("escriba un numero entero para realizar 4 operaciones basicas: ");
+        num = teclado.nextInt();
+        System.out.println("escriba el segundo numero entero: ");
+        num2= teclado.nextInt();
+        int resultadosum, res, mul,div;
+        resultadosum = num + num2;
+        res = num - num2;
+        mul = num * num2;
+        div = num / num2;
+        
+        System.out.println("el resultado de la suma sera: "+ resultadosum);
+        System.out.println("el resultado de la resta sera: "+ res);
+        System.out.println("el resultado de la multiplicacion sera: "+ mul);
+        System.out.println("el resultado de la divicion sera: "+ div);
+        
+    }
+    
+}
